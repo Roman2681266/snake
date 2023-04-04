97d4252d8e3e69b5eab8c0c8593b8cc0d2b40552
asdasdasd <asdasdasd>
2023-04-04 17:04:36 +0300

    moving

--- date.py
+++ date.py
@@ -1,8 +1,10 @@
 import pygame
+import random
+
 pygame.init()
 rect_size = 20
 field_size=20
-dis = pygame.display.set_mode((400,300))
+dis = pygame.display.set_mode((rect_size * field_size,rect_size * field_size))
 pygame.display.set_caption('Snake')
 game_close = False
 snake_direction = 'right'
@@ -13,3 +15,4 @@ green = (0,255,0)
 blue=(0,0,100)
 red=(255,0,0)
 black=(0,0,0)
+apple_position = {'x': 0, 'y': 0}
--- function.py
+++ function.py
@@ -7,20 +7,55 @@ def quit_game(): # Выйти из программы
     quit()
 def event():
     global game_close
+    global snake_direction
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             game_close = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                snake_direction = 'left'
+                print('left')
+            elif event.key == pygame.K_RIGHT:
+                snake_direction = 'right'
+                print('right')
+            elif event.key == pygame.K_UP:
+                snake_direction = 'up'
+                print('up')
+            elif event.key == pygame.K_DOWN:
+                snake_direction = 'down'
+                print('down')
 
 def simulation():
+    global snake_head
+    if snake_direction == 'left':
+        snake_head['x']-=1
+        snake_head['x'] %= field_size
     if snake_direction == 'right':
         snake_head['x']+=1
+        snake_head['x'] %= field_size
+    if snake_direction == 'up':
+        snake_head['y']-=1
+        snake_head['y'] %= field_size
+    if snake_direction == 'down':
+        snake_head['y']+=1
+        snake_head['y'] %= field_size
+    if snake_head == apple_position:
+        new_apple()
 def display():
     dis.fill(black)
     #pygame.draw.rect(dis,(255,255,255),(75,75,50,50))
+
     drawSnakeHead()
+    draw_apple()
     pygame.display.update()
     clock.tick(speed)
+
+
 def drawSnakeHead():
     global dis
     pygame.draw.rect(dis,green,(snake_head['x']*20,snake_head['y']*20,20,20))
+def draw_apple():
+    pygame.draw.rect(dis,red,(apple_position['x']*20,apple_position['y']*20,20,20))
+def new_apple():
+    apple_position['x'] = random.randint(0,field_size - 1)
+    apple_position['y'] = random.randint(0,field_size - 1)
